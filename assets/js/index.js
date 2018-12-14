0b7a24f1d4b9fdc43484080f48853879551dfdf7
Vladimir Masko <soupdawgie@gmail.com>
2018-12-14 14:32:40 +0300

    Add screenshots and slider

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".js-phone-slider").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    draggable: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
